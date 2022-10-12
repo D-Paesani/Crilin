69bf68064bbdb7538cbada2777c15eab1a59cfe2
D-Paesani <58980766+D-Paesani@users.noreply.github.com>
2022-10-12 12:11:57 +0200

    plots

--- H2_22/out/run530167/reso_2.C
+++ H2_22/out/run530167/reso_2.C
@@ -1,96 +1,96 @@
 void reso_2()
 {
 //=========Macro generated from canvas: c_1/c_1
-//=========  (Wed Oct 12 11:28:37 2022) by ROOT version 6.24/06
+//=========  (Wed Oct 12 12:10:39 2022) by ROOT version 6.24/06
    TCanvas *c_1 = new TCanvas("c_1", "c_1",0,0,600,450);
    gStyle->SetOptFit(1);
    c_1->SetHighLightColor(2);
-   c_1->Range(18.90836,-0.001347168,328.7296,0.03212451);
+   c_1->Range(18.86673,-0.001355343,328.7433,0.03219808);
    c_1->SetFillColor(0);
    c_1->SetBorderMode(0);
    c_1->SetBorderSize(2);
    c_1->SetFrameBorderMode(0);
    c_1->SetFrameBorderMode(0);
    
-   Double_t Graph0_fx1022[6] = {
-   72.69547,
-   112.9555,
-   153.0001,
-   193.8512,
-   234.1903,
-   274.7239};
-   Double_t Graph0_fy1022[6] = {
-   0.02682745,
-   0.01902874,
-   0.014868,
+   Double_t Graph0_fx1001[6] = {
+   72.66343,
+   112.9514,
+   152.9971,
+   193.8575,
+   234.1913,
+   274.7282};
+   Double_t Graph0_fy1001[6] = {
+   0.02688635,
+   0.01902855,
+   0.0148665,
    0.01268887,
-   0.01110502,
-   0.01018202};
-   Double_t Graph0_fex1022[6] = {
-   0.1252521,
-   0.108464,
-   0.1160687,
-   0.1455879,
-   0.2104007,
-   0.3439065};
-   Double_t Graph0_fey1022[6] = {
-   0.0002360915,
-   0.0001456229,
+   0.01110068,
+   0.0101802};
+   Double_t Graph0_fex1001[6] = {
+   0.1252653,
+   0.1084254,
+   0.1160505,
+   0.1456138,
+   0.2102707,
+   0.343635};
+   Double_t Graph0_fey1001[6] = {
+   0.0002364892,
+   0.0001456843,
    0.0001187636,
    0.0001256567,
-   0.0001732143,
-   0.0002565158};
-   TGraphErrors *gre = new TGraphErrors(6,Graph0_fx1022,Graph0_fy1022,Graph0_fex1022,Graph0_fey1022);
+   0.0001732021,
+   0.0002563452};
+   TGraphErrors *gre = new TGraphErrors(6,Graph0_fx1001,Graph0_fy1001,Graph0_fex1001,Graph0_fey1001);
    gre->SetName("Graph0");
    gre->SetTitle("run530167");
    gre->SetFillStyle(1000);
    gre->SetMarkerStyle(20);
    gre->SetMarkerSize(0.5);
    
-   TH1F *Graph_Graph01022 = new TH1F("Graph_Graph01022","run530167",100,52.32046,295.3175);
-   Graph_Graph01022->SetMinimum(0.002);
-   Graph_Graph01022->SetMaximum(0.02877734);
-   Graph_Graph01022->SetDirectory(0);
-   Graph_Graph01022->SetStats(0);
+   TH1F *Graph_Graph01001 = new TH1F("Graph_Graph01001","run530167",100,52.2848,295.3252);
+   Graph_Graph01001->SetMinimum(0.002);
+   Graph_Graph01001->SetMaximum(0.02884274);
+   Graph_Graph01001->SetDirectory(0);
+   Graph_Graph01001->SetStats(0);
 
    Int_t ci;      // for color index setting
    TColor *color; // for color definition with alpha
    ci = TColor::GetColor("#000099");
-   Graph_Graph01022->SetLineColor(ci);
-   Graph_Graph01022->GetXaxis()->SetTitle("mean SiPM charge [pC]");
-   Graph_Graph01022->GetXaxis()->SetRange(0,101);
-   Graph_Graph01022->GetXaxis()->SetLabelFont(42);
-   Graph_Graph01022->GetXaxis()->SetTitleOffset(1);
-   Graph_Graph01022->GetXaxis()->SetTitleFont(42);
-   Graph_Graph01022->GetYaxis()->SetTitle("mean time cell resolution [ns]");
-   Graph_Graph01022->GetYaxis()->SetLabelFont(42);
-   Graph_Graph01022->GetYaxis()->SetTitleFont(42);
-   Graph_Graph01022->GetZaxis()->SetLabelFont(42);
-   Graph_Graph01022->GetZaxis()->SetTitleOffset(1);
-   Graph_Graph01022->GetZaxis()->SetTitleFont(42);
-   gre->SetHistogram(Graph_Graph01022);
+   Graph_Graph01001->SetLineColor(ci);
+   Graph_Graph01001->GetXaxis()->SetTitle("mean SiPM charge [pC]");
+   Graph_Graph01001->GetXaxis()->SetRange(0,101);
+   Graph_Graph01001->GetXaxis()->SetLabelFont(42);
+   Graph_Graph01001->GetXaxis()->SetTitleOffset(1);
+   Graph_Graph01001->GetXaxis()->SetTitleFont(42);
+   Graph_Graph01001->GetYaxis()->SetTitle("mean time cell resolution [ns]");
+   Graph_Graph01001->GetYaxis()->SetLabelFont(42);
+   Graph_Graph01001->GetYaxis()->SetTitleFont(42);
+   Graph_Graph01001->GetZaxis()->SetLabelFont(42);
+   Graph_Graph01001->GetZaxis()->SetTitleOffset(1);
+   Graph_Graph01001->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph01001);
    
    
-   TF1 *fitf1023 = new TF1("fitf","sqrt([0]*[0]/(x*x) + [1]*[1])",52.32046,295.3175, TF1::EAddToList::kNo);
-   fitf1023->SetFillColor(19);
-   fitf1023->SetFillStyle(0);
-   fitf1023->SetLineColor(2);
-   fitf1023->SetLineWidth(2);
-   fitf1023->SetChisquare(17.3308);
-   fitf1023->SetNDF(4);
-   fitf1023->GetXaxis()->SetLabelFont(42);
-   fitf1023->GetXaxis()->SetTitleOffset(1);
-   fitf1023->GetXaxis()->SetTitleFont(42);
-   fitf1023->GetYaxis()->SetLabelFont(42);
-   fitf1023->GetYaxis()->SetTitleFont(42);
-   fitf1023->SetParameter(0,1.912281);
-   fitf1023->SetParError(0,0.01679948);
-   fitf1023->SetParLimits(0,0,0);
-   fitf1023->SetParameter(1,0.007885544);
-   fitf1023->SetParError(1,0.0001853192);
-   fitf1023->SetParLimits(1,0,0);
-   fitf1023->SetParent(gre);
-   gre->GetListOfFunctions()->Add(fitf1023);
+   TF1 *fitf1002 = new TF1("fitf","sqrt([0]*[0]/(x*x) + [1]*[1])",52.2848,295.3252, TF1::EAddToList::kNo);
+   fitf1002->SetFillColor(19);
+   fitf1002->SetFillStyle(0);
+   fitf1002->SetLineColor(2);
+   fitf1002->SetLineWidth(2);
+   fitf1002->SetChisquare(16.31629);
+   fitf1002->SetNDF(4);
+   fitf1002->GetXaxis()->SetLabelFont(42);
+   fitf1002->GetXaxis()->SetTitleOffset(1);
+   fitf1002->GetXaxis()->SetTitleFont(42);
+   fitf1002->GetYaxis()->SetLabelFont(42);
+   fitf1002->GetYaxis()->SetTitleFont(42);
+   fitf1002->SetParameter(0,1.914926);
+   fitf1002->SetParError(0,0.01679252);
+   fitf1002->SetParLimits(0,0,0);
+   fitf1002->SetParameter(1,0.007862912);
+   fitf1002->SetParError(1,0.0001858054);
+   fitf1002->SetParLimits(1,0,0);
+   fitf1002->SetParent(gre);
+   gre->GetListOfFunctions()->Add(fitf1002);
    
    TPaveStats *ptstats = new TPaveStats(0.62,0.815,0.98,0.935,"brNDC");
    ptstats->SetName("stats");
@@ -98,8 +98,8 @@ void reso_2()
    ptstats->SetFillColor(0);
    ptstats->SetTextAlign(12);
    ptstats->SetTextFont(42);
-   TText *ptstats_LaTex = ptstats->AddText("a [ns#bulletpC] = 1.91228 #pm 0.0167995 ");
-   ptstats_LaTex = ptstats->AddText("b [ns]   = 0.00788554 #pm 0.000185319 ");
+   TText *ptstats_LaTex = ptstats->AddText("a [ns#bulletpC] = 1.91493 #pm 0.0167925 ");
+   ptstats_LaTex = ptstats->AddText("b [ns]   = 0.00786291 #pm 0.000185805 ");
    ptstats->SetOptStat(0);
    ptstats->SetOptFit(11);
    ptstats->Draw();
--- H2_22/out/run530167/reso_3.C
+++ H2_22/out/run530167/reso_3.C
@@ -1,74 +1,74 @@
 void reso_3()
 {
 //=========Macro generated from canvas: c_2/c_2
-//=========  (Wed Oct 12 11:28:37 2022) by ROOT version 6.24/06
+//=========  (Wed Oct 12 12:10:40 2022) by ROOT version 6.24/06
    TCanvas *c_2 = new TCanvas("c_2", "c_2",0,0,600,450);
    gStyle->SetOptFit(1);
    c_2->SetHighLightColor(2);
-   c_2->Range(18.90836,-0.001347168,328.7296,0.03212451);
+   c_2->Range(18.86673,-0.001355343,328.7433,0.03219808);
    c_2->SetFillColor(0);
    c_2->SetBorderMode(0);
    c_2->SetBorderSize(2);
    c_2->SetFrameBorderMode(0);
    c_2->SetFrameBorderMode(0);
    
-   Double_t Graph0_fx1025[6] = {
-   72.69547,
-   112.9555,
-   153.0001,
-   193.8512,
-   234.1903,
-   274.7239};
-   Double_t Graph0_fy1025[6] = {
-   0.02682745,
-   0.01902874,
-   0.014868,
+   Double_t Graph0_fx1004[6] = {
+   72.66343,
+   112.9514,
+   152.9971,
+   193.8575,
+   234.1913,
+   274.7282};
+   Double_t Graph0_fy1004[6] = {
+   0.02688635,
+   0.01902855,
+   0.0148665,
    0.01268887,
-   0.01110502,
-   0.01018202};
-   Double_t Graph0_fex1025[6] = {
-   0.1252521,
-   0.108464,
-   0.1160687,
-   0.1455879,
-   0.2104007,
-   0.3439065};
-   Double_t Graph0_fey1025[6] = {
-   0.0002360915,
-   0.0001456229,
+   0.01110068,
+   0.0101802};
+   Double_t Graph0_fex1004[6] = {
+   0.1252653,
+   0.1084254,
+   0.1160505,
+   0.1456138,
+   0.2102707,
+   0.343635};
+   Double_t Graph0_fey1004[6] = {
+   0.0002364892,
+   0.0001456843,
    0.0001187636,
    0.0001256567,
-   0.0001732143,
-   0.0002565158};
-   TGraphErrors *gre = new TGraphErrors(6,Graph0_fx1025,Graph0_fy1025,Graph0_fex1025,Graph0_fey1025);
+   0.0001732021,
+   0.0002563452};
+   TGraphErrors *gre = new TGraphErrors(6,Graph0_fx1004,Graph0_fy1004,Graph0_fex1004,Graph0_fey1004);
    gre->SetName("Graph0");
    gre->SetTitle("run530167");
    gre->SetFillStyle(1000);
    gre->SetMarkerStyle(20);
    gre->SetMarkerSize(0.5);
    
-   TH1F *Graph_Graph_Graph010221025 = new TH1F("Graph_Graph_Graph010221025","run530167",100,52.32046,295.3175);
-   Graph_Graph_Graph010221025->SetMinimum(0.002);
-   Graph_Graph_Graph010221025->SetMaximum(0.02877734);
-   Graph_Graph_Graph010221025->SetDirectory(0);
-   Graph_Graph_Graph010221025->SetStats(0);
+   TH1F *Graph_Graph_Graph010011004 = new TH1F("Graph_Graph_Graph010011004","run530167",100,52.2848,295.3252);
+   Graph_Graph_Graph010011004->SetMinimum(0.002);
+   Graph_Graph_Graph010011004->SetMaximum(0.02884274);
+   Graph_Graph_Graph010011004->SetDirectory(0);
+   Graph_Graph_Graph010011004->SetStats(0);
 
    Int_t ci;      // for color index setting
    TColor *color; // for color definition with alpha
    ci = TColor::GetColor("#000099");
-   Graph_Graph_Graph010221025->SetLineColor(ci);
-   Graph_Graph_Graph010221025->GetXaxis()->SetTitle("mean SiPM charge [pC]");
-   Graph_Graph_Graph010221025->GetXaxis()->SetRange(0,101);
-   Graph_Graph_Graph010221025->GetXaxis()->SetLabelFont(42);
-   Graph_Graph_Graph010221025->GetXaxis()->SetTitleOffset(1);
-   Graph_Graph_Graph010221025->GetXaxis()->SetTitleFont(42);
-   Graph_Graph_Graph010221025->GetYaxis()->SetTitle("mean time cell resolution [ns]");
-   Graph_Graph_Graph010221025->GetYaxis()->SetLabelFont(42);
-   Graph_Graph_Graph010221025->GetYaxis()->SetTitleFont(42);
-   Graph_Graph_Graph010221025->GetZaxis()->SetLabelFont(42);
-   Graph_Graph_Graph010221025->GetZaxis()->SetTitleOffset(1);
-   Graph_Graph_Graph010221025->GetZaxis()->SetTitleFont(42);
-   gre->SetHistogram(Graph_Graph_Graph010221025);
+   Graph_Graph_Graph010011004->SetLineColor(ci);
+   Graph_Graph_Graph010011004->GetXaxis()->SetTitle("mean SiPM charge [pC]");
+   Graph_Graph_Graph010011004->GetXaxis()->SetRange(0,101);
+   Graph_Graph_Graph010011004->GetXaxis()->SetLabelFont(42);
+   Graph_Graph_Graph010011004->GetXaxis()->SetTitleOffset(1);
+   Graph_Graph_Graph010011004->GetXaxis()->SetTitleFont(42);
+   Graph_Graph_Graph010011004->GetYaxis()->SetTitle("mean time cell resolution [ns]");
+   Graph_Graph_Graph010011004->GetYaxis()->SetLabelFont(42);
+   Graph_Graph_Graph010011004->GetYaxis()->SetTitleFont(42);
+   Graph_Graph_Graph010011004->GetZaxis()->SetLabelFont(42);
+   Graph_Graph_Graph010011004->GetZaxis()->SetTitleOffset(1);
+   Graph_Graph_Graph010011004->GetZaxis()->SetTitleFont(42);
+   gre->SetHistogram(Graph_Graph_Graph010011004);
    
    
    TPaveStats *ptstats = new TPaveStats(0.62,0.815,0.98,0.935,"brNDC");
@@ -77,38 +77,38 @@ void reso_3()
    ptstats->SetFillColor(0);
    ptstats->SetTextAlign(12);
    ptstats->SetTextFont(42);
-   TText *ptstats_LaTex = ptstats->AddText("a [ns#bulletpC] = 1.62316 #pm 0.0849463 ");
-   ptstats_LaTex = ptstats->AddText("b [ns#bulletpC^{0.5}] = 0.126103 #pm 0.0165978 ");
-   ptstats_LaTex = ptstats->AddText("c [ns]   = 0.00286686 #pm 0.00252315 ");
+   TText *ptstats_LaTex = ptstats->AddText("a [ns#bulletpC] = 1.63567 #pm 0.0841535 ");
+   ptstats_LaTex = ptstats->AddText("b [ns#bulletpC^{0.5}] = 0.12418 #pm 0.0168151 ");
+   ptstats_LaTex = ptstats->AddText("c [ns]   = 0.00308393 #pm 0.00233949 ");
    ptstats->SetOptStat(0);
    ptstats->SetOptFit(11);
    ptstats->Draw();
    gre->GetListOfFunctions()->Add(ptstats);
    ptstats->SetParent(gre->GetListOfFunctions());
    
-   TF1 *fitf1027 = new TF1("fitf","sqrt([0]*[0]/(x*x) + [1]*[1]/x + [2]*[2])",49.89049,297.7475, TF1::EAddToList::kNo);
-   fitf1027->SetFillColor(19);
-   fitf1027->SetFillStyle(0);
-   fitf1027->SetLineColor(2);
-   fitf1027->SetLineWidth(2);
-   fitf1027->SetChisquare(3.023198);
-   fitf1027->SetNDF(3);
-   fitf1027->GetXaxis()->SetLabelFont(42);
-   fitf1027->GetXaxis()->SetTitleOffset(1);
-   fitf1027->GetXaxis()->SetTitleFont(42);
-   fitf1027->GetYaxis()->SetLabelFont(42);
-   fitf1027->GetYaxis()->SetTitleFont(42);
-   fitf1027->SetParameter(0,1.623157);
-   fitf1027->SetParError(0,0.0849463);
-   fitf1027->SetParLimits(0,0,0);
-   fitf1027->SetParameter(1,0.1261033);
-   fitf1027->SetParError(1,0.01659777);
-   fitf1027->SetParLimits(1,0,0);
-   fitf1027->SetParameter(2,0.002866858);
-   fitf1027->SetParError(2,0.002523153);
-   fitf1027->SetParLimits(2,0,0);
-   fitf1027->SetParent(gre);
-   gre->GetListOfFunctions()->Add(fitf1027);
+   TF1 *fitf1006 = new TF1("fitf","sqrt([0]*[0]/(x*x) + [1]*[1]/x + [2]*[2])",49.85439,297.7557, TF1::EAddToList::kNo);
+   fitf1006->SetFillColor(19);
+   fitf1006->SetFillStyle(0);
+   fitf1006->SetLineColor(2);
+   fitf1006->SetLineWidth(2);
+   fitf1006->SetChisquare(2.864768);
+   fitf1006->SetNDF(3);
+   fitf1006->GetXaxis()->SetLabelFont(42);
+   fitf1006->GetXaxis()->SetTitleOffset(1);
+   fitf1006->GetXaxis()->SetTitleFont(42);
+   fitf1006->GetYaxis()->SetLabelFont(42);
+   fitf1006->GetYaxis()->SetTitleFont(42);
+   fitf1006->SetParameter(0,1.635671);
+   fitf1006->SetParError(0,0.08415347);
+   fitf1006->SetParLimits(0,0,0);
+   fitf1006->SetParameter(1,0.1241801);
+   fitf1006->SetParError(1,0.01681511);
+   fitf1006->SetParLimits(1,0,0);
+   fitf1006->SetParameter(2,0.003083926);
+   fitf1006->SetParError(2,0.002339492);
+   fitf1006->SetParLimits(2,0,0);
+   fitf1006->SetParent(gre);
+   gre->GetListOfFunctions()->Add(fitf1006);
    gre->Draw("ap");
    TGaxis *gaxis = new TGaxis(60,0.006,290,0.006,148.8,719.2,510,"-");
    gaxis->SetLabelOffset(0.005);
